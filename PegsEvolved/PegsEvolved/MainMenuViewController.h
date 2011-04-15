b95ea6ee6ac490a27d260a4b82b05102e625eb1f
Billy <billyjoe0395@aim.com>
2011-04-14 20:56:13 -0400

    adding structure

---
+++ PegsEvolved/PegsEvolved/MainMenuViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainMenuViewController.h
+//  PegsEvolved
+//
+//  Created by Billy Connolly on 4/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainMenuViewController : UIViewController {
+    
+}
+
+@end
